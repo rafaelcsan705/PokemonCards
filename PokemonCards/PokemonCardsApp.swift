d817f4b648f3ec2bb476df7793ec16a6e1f1fc3f
Rafael C Santos <rafaelcsan705@gmail.com>
2025-03-23 16:27:30 +0000

    Initial Commit

---
+++ PokemonCards/PokemonCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCardsApp.swift
+//  PokemonCards
+//
+//  Created by Rafael Santos on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonCardsTests/PokemonCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCardsTests.swift
+//  PokemonCardsTests
+//
+//  Created by Rafael Santos on 23/03/2025.
+//
+
+import Testing
+@testable import PokemonCards
+
+struct PokemonCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
